95bcf55f2a6863b6293fa51595cce596333c6669
Mirjaloliddin <murtazaev_26@icloud.com>
2023-02-25 21:46:31 +0500

    commit

--- MyFirstProject/MyFirstProject/ViewController.swift
+++ MyFirstProject/MyFirstProject/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let label = UILabel()
         
         view.backgroundColor = .darkGray
 
